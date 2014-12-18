31a48585e1a41878ea4d766db32acc755175adb2
git+ssh://pi@humladoor/home/pi/.git <root@hackeriet.no>
2014-12-18 15:06:37 +0100

    autossh config is in current dir

--- bellhost/doorconn.sh
+++ bellhost/doorconn.sh
@@ -7,8 +7,8 @@ AUTOSSH_LOGFILE=autossh.log
 AUTOSSH_PATH=/usr/bin/ssh
 SSHOST=user@webhost
 SSHID=id_rsa
-[ -f autossh.config ] && . autossh.config
+[ -f autossh.config ] && . ./autossh.config
 
 export AUTOSSH_POLL AUTOSSH_PORT AUTOSSH_GATETIME AUTOSSH_LOGFILE AUTOSSH_PATH
-autossh -2 -fN -M 20000 -R8101:localhost:8000 -R5000:localhost:5000 -i $SSHID $SSHOST
+autossh -2 -N -M 20000 -R8101:localhost:8000 -R5000:localhost:5000 -i $SSHID $SSHOST $@
 
